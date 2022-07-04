51495ccd450f625bcb687ffa8d1e86877311fdd2
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-04 16:11:15 +0430

    Pass test case to assert that if one number is provided, it will be returned as result.

--- src/main/kotlin/com/tdd/StringCalculator.kt
+++ src/main/kotlin/com/tdd/StringCalculator.kt
@@ -5,6 +5,8 @@ class StringCalculator {
     fun calculate(inputs: String): Long {
         if (inputs.trim() == "")
             return 0
+        if (inputs.toList().size < 3)
+            return java.lang.Long.valueOf(inputs.toList()[0].toString())
         return java.lang.Long.valueOf(inputs.toList()[0].toString())
             .plus(java.lang.Long.valueOf(inputs.toList()[2].toString()))
     }
