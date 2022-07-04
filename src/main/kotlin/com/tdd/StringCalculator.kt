6cb204d36f8b6f2e9ae590853126cdc77896f18d
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-04 15:59:24 +0430

    Pass test case to assert that if input is empty string, 0 will be returned.

--- src/main/kotlin/com/tdd/StringCalculator.kt
+++ src/main/kotlin/com/tdd/StringCalculator.kt
@@ -3,6 +3,8 @@ package com.tdd
 class StringCalculator {
 
     fun calculate(inputs: String): Long {
+        if (inputs.trim() == "")
+            return 0
         return java.lang.Long.valueOf(inputs.toList()[0].toString())
             .plus(java.lang.Long.valueOf(inputs.toList()[2].toString()))
     }
