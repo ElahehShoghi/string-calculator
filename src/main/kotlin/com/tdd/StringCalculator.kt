b27a2808a0e8ac49802f74c0c62ded058ea854ee
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-04 16:21:22 +0430

    Refactor calculate method by extract a private function.

--- src/main/kotlin/com/tdd/StringCalculator.kt
+++ src/main/kotlin/com/tdd/StringCalculator.kt
@@ -5,9 +5,13 @@ class StringCalculator {
     fun calculate(inputs: String): Long {
         if (inputs.trim() == "")
             return 0
+        val firstNumber = getValueFromListByIndex(inputs.toList(), 0)
         if (inputs.toList().size < 3)
-            return java.lang.Long.valueOf(inputs.toList()[0].toString())
-        return java.lang.Long.valueOf(inputs.toList()[0].toString())
-            .plus(java.lang.Long.valueOf(inputs.toList()[2].toString()))
+            return firstNumber
+        return firstNumber.plus(getValueFromListByIndex(inputs.toList(), 2))
+    }
+
+    private fun getValueFromListByIndex(list: List<Char>, index: Int): Long {
+        return java.lang.Long.valueOf(list[index].toString())
     }
 }
